0ff4986f8984c7ebe61bd227037bccf4ca252b34
Anand kumar Bandaru <163977432+Anand119@users.noreply.github.com>
2026-01-18 16:31:52 +0530

    Add files via upload

---
+++ contact-form-with-custom-image-captcha/contact-form-with-custom-image-captcha/Model/Contact.php
@@ -0,0 +1,54 @@
+<?php
+namespace Phppot;
+
+use Phppot\DataSource;
+
+class Contact
+{
+
+    private $ds;
+
+    function __construct()
+    {
+        require_once __DIR__ . './../lib/DataSource.php';
+        $this->ds = new DataSource();
+    }
+
+    function getRecord()
+    {
+        $query = "SELECT * FROM tbl_term ORDER BY RAND() LIMIT 1";
+
+        $result = $this->ds->select($query);
+        return $result;
+    }
+
+    function getCaptchaIcons($id)
+    {
+        $query = "SELECT tbl_captcha_images.*, tbl_term.name FROM tbl_captcha_images 
+        JOIN tbl_term ON tbl_captcha_images.term_id = tbl_term.id WHERE term_id != " . $id . " ORDER BY RAND() LIMIT 4";
+
+        $captchaResult = $this->ds->select($query);
+        return $captchaResult;
+    }
+
+    function getRandomCaptchaId($id)
+    {
+        $query = "SELECT tbl_captcha_images.*, tbl_term.name FROM tbl_captcha_images 
+        JOIN tbl_term ON tbl_captcha_images.term_id = tbl_term.id WHERE term_id = " . $id . " ORDER BY RAND() LIMIT 1";
+        $captcha = $this->ds->select($query);
+        return $captcha;
+    }
+
+    function sendContactMail($postValues)
+    {
+        $name = $postValues["userName"];
+        $email = $postValues["userEmail"];
+        $subject = $postValues["subject"];
+        $content = $postValues["content"];
+
+        $toEmail = "ADMIN_EMAIL";
+        $mailHeaders = "From: " . $name . "<" . $email . ">\r\n";
+        $response = mail($toEmail, $subject, $content, $mailHeaders);
+        return $response;
+    }
+}
